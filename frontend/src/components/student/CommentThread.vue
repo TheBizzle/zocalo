TheBizzle/zocalo

commit de4d34da3e7f4a896d55259761b216bfd9263046

Refactor/Major: Remove the "<adjective> <animal>" naming system

frontend/src/components/student/SubmissionDetailModal.vue
@@ -6,14 +6,14 @@
     <div class="modal-box item-detail-modal animate-scale">
       <button class="btn-icon modal-close" @click="deactivate">✕</button>
       <h2 style="margin-bottom: var(--space-2)">
-        [{{ submission.uploader }}] {{ submission.uploadName }}
+        {{ submission.uploader }}
       </h2>
 
       <div class="scroll-pane">
 
         <div class="item-preview">
           <img v-if="submission?.image" :src="submission?.image"
-               :alt="submission?.uploadName" style="max-width: 100%; border-radius: var(--radius-md)" />
+               :alt="submission?.uploader" style="max-width: 100%; border-radius: var(--radius-md)" />
           <div v-else class="no-thumb-large">📄</div>
         </div>
 
@@ -25,8 +25,8 @@
 
         <div class="item-actions">
           <a class="btn btn-primary btn-lg dl-button"
-             :href="`/api/galleries/${galleryID}/student/${submission.uploadName}`"
-             :download="submission.uploadName">↓ Download</a>
+             :href="`/api/galleries/${galleryID}/student/${submission.id}`"
+             :download="submission.uploader">↓ Download</a>
           <button class="btn btn-primary btn-lg" @click="loadInSplit">
             <span class="dl-icon">👀</span> Load
           </button>
@@ -34,7 +34,7 @@
 
         <hr class="divider" />
         <CommentThread :comments="submission.comments" :galleryID="galleryID"
-                       :submissionName="submission.uploadName" />
+                       :submissionName="submission.uploader" />
 
       </div>
 

frontend/src/components/student/BasicGallery.vue
@@ -14,12 +14,12 @@
         @click="openItem(item)"
       >
         <div class="gallery-item-thumb">
-          <img v-if="item.image" :src="item.image" :alt="item.uploadName" />
+          <img v-if="item.image" :src="item.image" :alt="item.uploader" />
           <div v-else class="no-thumb">📄</div>
         </div>
         <div class="gallery-item-info">
           <div class="gallery-item-title">
-            [{{ item.uploader }}] {{ item.uploadName }}
+            [{{ item.uploader }}] {{ item.uploader }}
           </div>
           <div class="gallery-item-meta">
             {{ item.comments.length }} comment{{ item.comments.length !== 1 ? 's' : '' }}

frontend/src/components/student/UploadModal.vue
@@ -278,7 +278,6 @@
 
             const submission =
               { id:           response.id
-              , uploadName:   response.name
               , data
               , image
               , isOwner:      true

frontend/src/core/Submission.ts
@@ -16,7 +16,6 @@ const SubmissionSchema =
   z.object(
     { id:           z.number()
     , data:         z.string().nullish()
-    , uploadName:   z.string()
     , uploader:     z.string()
     , image:        z.base64().transform((s) => `data:image/png;base64,${s}`)
     , isOwner:      z.boolean()

frontend/src/views/SplitGalleryView.vue
@@ -149,7 +149,7 @@
       }
 
       async function loadInSplit(submission: Submission): Promise<void> {
-        const res = await fetch(`/api/galleries/${galleryID.value}/student/${submission.uploadName}`);
+        const res = await fetch(`/api/galleries/${galleryID.value}/student/${submission.id}`);
         if (!res.ok) {
           const message = await res.text();
           alert(`Could not load item: ${message}`);

src/main/Zocalo/BadgerState/Controller.hs
@@ -8,7 +8,7 @@ import qualified Data.UUID as UUID
 import Snap.Core(Method(GET, POST), Snap, writeText)
 import Snap.Util.GZip(withCompression)
 
-import Zocalo.Common.SnapHelpers(allowingCORS, Arg(Arg), asNonNegInt, asUUID, encodeText, failWith, handle1, handle2, handle3, notEmpty, succeed)
+import Zocalo.Common.SnapHelpers(allowingCORS, Arg(Arg), asNonNeg, asUUID, encodeText, failWith, handle1, handle2, handle3, notEmpty, succeed)
 
 import Zocalo.BadgerState.Database(joinGroup, readDataFor, readGroup, readNDataFor, readSignalFor, writeData, writeSignal)
 import Zocalo.BadgerState.Datum(Datum(Datum))
@@ -40,12 +40,12 @@ handlePostData =
 
 handleFetchData :: Snap ()
 handleFetchData =
-  handle3 (Arg "group-id" notEmpty, Arg "bucket-id" asUUID, Arg "ts" asNonNegInt) $
-    (\(g, b, t) -> readDataFor g b $ intToTime t) &> liftIO &>= ((map mkDatum) &> encodeText &> (succeed "application/json"))
+  handle3 (Arg "group-id" notEmpty, Arg "bucket-id" asUUID, Arg "ts" asNonNeg) $
+    (\(g, b, t) -> readDataFor g b $ wordToTime t) &> liftIO &>= ((map mkDatum) &> encodeText &> (succeed "application/json"))
 
 handleFetchNData :: Snap ()
 handleFetchNData =
-  handle3 (Arg "group-id" notEmpty, Arg "bucket-id" asUUID, Arg "n" asNonNegInt) $
+  handle3 (Arg "group-id" notEmpty, Arg "bucket-id" asUUID, Arg "n" asNonNeg) $
     (uncurry3 readNDataFor) &> liftIO &>= ((map mkDatum) &> encodeText &> (succeed "application/json"))
 
 handlePostSignal :: Snap ()
@@ -70,5 +70,5 @@ timeToText = timeToInteger &> showText
 timeToInteger :: UTCTime -> Integer
 timeToInteger = utcTimeToPOSIXSeconds &> (* 1e6) &> floor
 
-intToTime :: Int -> UTCTime
-intToTime = fromIntegral &> (* 1e-6) &> posixSecondsToUTCTime
+wordToTime :: Word64 -> UTCTime
+wordToTime = fromIntegral &> (* 1e-6) &> posixSecondsToUTCTime

src/main/Zocalo/BadgerState/Database.hs
@@ -69,10 +69,10 @@ readDataFor groupID bucketID refTime = withDB $
     rows <- selectList [DataDBGroupID ==. (Text.toLower groupID), DataDBBucketID ==. (UUID.toText bucketID)] [Desc DataDBDateAdded]
     return $ rows >>= (entityVal &> (\(DataDB _ _ dataT time) -> if time `isNewerThan` refTime then [(dataT, time)] else []))
 
-readNDataFor :: Text -> UUID -> Int -> IO [(Text, UTCTime)]
+readNDataFor :: Text -> UUID -> Word64 -> IO [(Text, UTCTime)]
 readNDataFor groupID bucketID n = withDB $
   do
-    rows <- selectList [DataDBGroupID ==. (Text.toLower groupID), DataDBBucketID ==. (UUID.toText bucketID)] [Desc DataDBDateAdded, LimitTo n]
+    rows <- selectList [DataDBGroupID ==. (Text.toLower groupID), DataDBBucketID ==. (UUID.toText bucketID)] [Desc DataDBDateAdded, LimitTo $ fromIntegral n]
     return $ map (entityVal &> (\(DataDB _ _ dataT time) -> (dataT, time))) rows
 
 readGroup :: Text -> IO [UUID]

src/main/Zocalo/Common/SnapHelpers.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TupleSections #-}
-module Zocalo.Common.SnapHelpers(allowingCORS, Arg(Arg), asBool, asBytes, asInt, asNanoID, asNonNegInt, asToken, asUUID, Constraint(Constraint), decodeText, encodeText, failWith, free, getParamV, getParamVM, handle1, handle2, handle3, handle4, handle5, handle6, notEmpty, notifyBadParams, ok, succeed, withFileUploads) where
+module Zocalo.Common.SnapHelpers(allowingCORS, Arg(Arg), asBool, asBytes, asInt, asNanoID, asNonNeg, asToken, asUUID, Constraint(Constraint), decodeText, encodeText, failWith, free, getParamV, getParamVM, handle1, handle2, handle3, handle4, handle5, handle6, notEmpty, notifyBadParams, ok, succeed, withFileUploads) where
 
 import Codec.Compression.Zlib.Internal(decompressST, defaultDecompressParams, foldDecompressStreamWithInput, gzipFormat)
 
@@ -154,8 +154,8 @@ asNanoID = Constraint $ buildConstraint $
   where
     allowed = asText $ BSChar.unpack $ unAlphabet defaultAlphabet
 
-asNonNegInt :: Constraint Int
-asNonNegInt = Constraint $ buildConstraint $ \x -> (readMaybe (asString x) :: Maybe Int) |>
+asNonNeg :: Constraint Word64
+asNonNeg = Constraint $ buildConstraint $ \x -> (readMaybe (asString x) :: Maybe Word64) |>
   (>>= (\n -> if n < 0 then Nothing else Just n))
 
 asInt :: Constraint Int

commit 2f4e8f2abb084995efdd7bd8b75371b6a9eed337

Bugfix: Make comments work again
Probably a regression from de4d34d

frontend/src/components/student/CommentThread.vue
@@ -50,9 +50,9 @@
   export default defineComponent({
     name: "CommentThread"
   , props: {
-      comments:       { type: Array as PropType<Array<Comment>>, default: () => [] }
-    , galleryID:      { type: String, required: true }
-    , submissionName: { type: String, required: true }
+      comments:     { type: Array as PropType<Array<Comment>>, default: () => [] }
+    , galleryID:    { type: String, required: true }
+    , submissionID: { type: Number, required: true }
     }
   , setup(props) {
 
@@ -84,7 +84,7 @@
           postData.append("comment", comment);
           const options = { method: "POST", body: postData };
 
-          const url = `/api/galleries/${props.galleryID}/student/${props.submissionName}/comment`;
+          const url = `/api/galleries/${props.galleryID}/student/${props.submissionID}/comment`;
           const res = await authorizedFetch(url, options);
 
           if (res.ok) {

frontend/src/components/student/SubmissionDetailModal.vue
@@ -34,7 +34,7 @@
 
         <hr class="divider" />
         <CommentThread :comments="submission.comments" :galleryID="galleryID"
-                       :submissionName="submission.uploader" />
+                       :submissionID="submission.id" />
 
       </div>
 